bf30e68e71c440db0d9bf4e6632a7e14b10eb447
vmiller <vmiller>
2021-09-18 06:18:17 +0000

    Updates

--- main.tf
+++ main.tf
@@ -10,34 +10,12 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
   region  = "us-east-1"
 }
 
-resource "aws_instance" "web1" {
-  ami           = "${data.aws_ami.latest-amzn2.id}"
-  instance_type = "t2.micro"
-  security_groups = [ "allow_www_sg1", "allow_ssh_sg1", "default" ]
-  key_name      = var.keyname
-
-  tags = {
-    Name = var.instance_name
-  }
-}
-
-resource "aws_instance" "web2" {
-  ami           = "${data.aws_ami.latest-amzn2.id}"
-  instance_type = "t2.micro"
-  security_groups = [ "allow_www_sg1", "allow_ssh_sg1", "default" ]
-  key_name      = var.keyname
-
-  tags = {
-    Name = var.instance_name
-  }
-}
-
 resource "aws_security_group" "allow_www_sg1" {
-  name = "allow_www_sg1"
+  name        = "allow_www_sg1"
+  description = "Allow inbound web traffic"
 
   ingress {
     from_port   = 80
@@ -51,16 +29,22 @@ resource "aws_security_group" "allow_www_sg1" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags  = {
+    Name = "allow_www_sg1"
+  }
 }
 
 resource "aws_security_group" "allow_ssh_sg1" {
-  name = "allow_ssh_sg1"
+  name        = "allow_ssh_sg1"
+  description = "Allow inbound ssh traffic"
 
   ingress {
     from_port   = 22
@@ -68,11 +52,56 @@ resource "aws_security_group" "allow_ssh_sg1" {
     protocol    = "tcp"
     cidr_blocks = [ var.selfa, var.selfb ]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_sg1"
+  }
+}
+
+resource "aws_network_interface" "eni1" {
+  subnet_id = "subnet-d64e26b2"
+  security_groups = [ "allow_www_sg1", "allow_ssh_sg1", "ssh_web_sg" ]
+}
+
+resource "aws_network_interface" "eni2" {
+  subnet_id = "subnet-4490356b"
+  security_groups = [ "allow_www_sg1", "allow_ssh_sg1", "ssh_web_sg" ]
+}
+
+resource "aws_instance" "web1" {
+  ami           = "${data.aws_ami.latest-amzn2.id}"
+  instance_type = "t2.micro"
+  key_name      = var.keyname
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = "${aws_network_interface.eni1.id}"
+  }
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami           = "${data.aws_ami.latest-amzn2.id}"
+  instance_type = "t2.micro"
+  key_name      = var.keyname
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = "${aws_network_interface.eni2.id}"
+  }
+
+  tags = {
+    Name = var.instance_name
   }
 }
 
