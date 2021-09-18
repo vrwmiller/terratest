252eb59f0001f8d278dfbc82fae9361180a06f11
vmiller <vmiller>
2021-09-18 14:53:17 +0000

    Add alb

--- main.tf
+++ main.tf
@@ -105,6 +105,63 @@ resource "aws_instance" "web2" {
   }
 }
 
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "alb1"
+
+  load_balancer_type = "application"
+
+  vpc_id             = "vpc-fe066986"
+  subnets            = ["subnet-d64e26b2", "subnet-4490356b"]
+  security_groups    = [ "${aws_security_group.allow_www_sg1.id}" ]
+
+  #access_logs = {
+  #  bucket = var.lblogs
+  #}
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = [
+        {
+          target_id = "${aws_instance.web1.id}"
+          port = 80
+        },
+        {
+          target_id = "${aws_instance.web2.id}"
+          port = 80
+        }
+      ]
+    }
+  ]
+
+  #https_listeners = [
+  #  {
+  #    port               = 443
+  #    protocol           = "HTTPS"
+  #    certificate_arn    = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+  #    target_group_index = 0
+  #  }
+  #]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Test"
+  }
+}
+
 data "aws_ami" "latest-amzn2" {
   most_recent = true
   owners = ["137112412989"]
